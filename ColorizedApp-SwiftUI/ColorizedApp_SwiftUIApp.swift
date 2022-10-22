0b81bf504ec1832e4da2e1ca1e643922c34df13c
Pashalazarev <mymessagebox9@gmail.com>
2022-10-22 12:53:05 +0300

    Initial Commit

---
+++ ColorizedApp-SwiftUI/ColorizedApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_SwiftUIApp.swift
+//  ColorizedApp-SwiftUI
+//
+//  Created by Pavel Lazarev Macbook on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
