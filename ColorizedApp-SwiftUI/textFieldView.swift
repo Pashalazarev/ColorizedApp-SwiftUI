973a4e3b0f4b451f8d7ea533acf9658bb84aa0d5
Pashalazarev <mymessagebox9@gmail.com>
2022-10-24 00:04:59 +0300

    Created: ColorView, slider, textField, text. Data from slider is being transmitted to text

---
+++ ColorizedApp-SwiftUI/textFieldView.swift
@@ -0,0 +1,20 @@
+//
+//  textFieldView.swift
+//  ColorizedApp-SwiftUI
+//
+//  Created by Pavel Lazarev Macbook on 24.10.2022.
+//
+
+import SwiftUI
+
+struct textFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct textFieldView_Previews: PreviewProvider {
+    static var previews: some View {
+        textFieldView()
+    }
+}
